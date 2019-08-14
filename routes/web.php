c3dce9d2662ec940c79c99f814e3b857594dee85
mayumi <mayumi@asadigital.net>
2019-08-14 11:51:17 +0800

    一覧のルートとコントローラーを作成

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'DiaryController@index')->name('diary.index'); //追加
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
