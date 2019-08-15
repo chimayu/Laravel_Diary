3ed5de09c0461c762abd9a440a7370d0dc7a354f
mayumi <mayumi@asadigital.net>
2019-08-15 11:05:51 +0800

    モデル・コントローラー・ビュー作成、一覧表示実装

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Diary; // App/Diaryクラスを使用する宣言
 use Illuminate\Http\Request;
 
 class DiaryController extends Controller
 {
     public function index()
     {
-        return 'Hello World';
+        //diariesテーブルのデータを全件取得
+        //useしてるDiaryのallメソッドを実施
+        //all()はテーブルのデータを全て取得するメソッド
+        $diaries = Diary::all(); 
+        // $diaries = []; 
+
+        // dd($diaries);  //var_dump()とdie()を合わせたメソッド。変数の確認 + 処理のストップ
+
+        // view/diaries/index.blade.phpを表示
+        // return view('diaries.index');
+        return view('diaries.index',['diaries' => $diaries]);
     }
 }
